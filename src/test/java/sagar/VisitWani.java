570dbe663eb610111b98002eeb65233b478d8bbc
sgrnkm11 <sgrnkm11@users.noreply.github.com>
2021-04-29 10:59:48 +0530

    Structured Code

--- src/test/java/sagar/VisitWani.java
+++ src/test/java/sagar/VisitWani.java
@@ -3,15 +3,25 @@ package sagar;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class VisitWani {
+    WebDriver driver;
+    @BeforeTest
+    public void OpenBrowser(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
     @Test
     public void testWani() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
         driver.get("http://wanisheth.com");
         System.out.println(driver.getTitle());
+    }
+    @AfterTest
+    public void closeBrowser(){
         driver.quit();
     }
 }
