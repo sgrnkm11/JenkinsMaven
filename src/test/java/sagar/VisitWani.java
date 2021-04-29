ddfc07236903214ad59d1040046c18fee9130be7
sgrnkm11 <sgrnkm11@users.noreply.github.com>
2021-04-29 11:13:02 +0530

    Addting true and false condition to test

--- src/test/java/sagar/VisitWani.java
+++ src/test/java/sagar/VisitWani.java
@@ -3,6 +3,7 @@ package sagar;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -19,6 +20,16 @@ public class VisitWani {
     public void testWani() {
         driver.get("http://wanisheth.com");
         System.out.println(driver.getTitle());
+    }
+    @Test(priority = 1)
+    public void testTitleSite(){
+       String title= driver.getTitle();
+       Assert.assertEquals(title, "Wanisheth - Home");
+    }
+    @Test(priority = 2)
+    public void testTitle(){
+       String title= driver.getTitle();
+       Assert.assertEquals(title, "Wanisheth -Home");
     }
     @AfterTest
     public void closeBrowser(){
